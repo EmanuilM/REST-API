ce70ea1c36da4c5b68315f2609b6a74be4ff1014
ExGiX <emanuilmladenov7@gmail.com>
2021-06-15 17:00:57 +0300

    test

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('./config');
-mongoose.connect(config.DB_CONNECTION , { useNewUrlParser: true  , useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://admin:<admin>@test.ijzqf.mongodb.net/testDB?retryWrites=true&w=majority' , { useNewUrlParser: true  , useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error' , console.error.bind(console, 'connection error'));
