04d7b4fa065211c0091951cc2241237c486ffe14
ExGiX <emanuilmladenov7@gmail.com>
2021-06-15 15:54:26 +0300

    test

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ require('./config/db');
 require('./config/express')(app);
 
 console.log(process.env.NODE_ENV);
-
+app.use(express.static(process.cwd()+"/angular-src/dist/testMaterial/"));
 app.use(cors({
     origin: config.origin,
     credentials: true,
 }))
 app.use(router);
+
+
+app.get('/', (req,res) => {
+    res.sendFile(process.cwd()+"/angular-src/dist/testMaterial/index.html");
+  });
 
 console.log(`Current port is ${process.env.PORT}`)
 app.listen(process.env.PORT , () => console.log(`Server is listening on port ${process.env.PORT}`));
