52b7204712808d151e4f13d016ffd2ebf3466cfd
ExGiX <emanuilmladenov7@gmail.com>
2021-06-15 17:04:03 +0300

    test

--- config/db.js
+++ config/db.js
@@ -1,7 +1,16 @@
-const mongoose = require('mongoose');
-const config = require('./config');
-mongoose.connect('mongodb+srv://admin:<admin>@test.ijzqf.mongodb.net/testDB?retryWrites=true&w=majority' , { useNewUrlParser: true  , useUnifiedTopology: true});
+// const mongoose = require('mongoose');
+// const config = require('./config');
+// mongoose.connect('mongodb+srv://admin:<admin>@test.ijzqf.mongodb.net/testDB?retryWrites=true&w=majority' , { useNewUrlParser: true  , useUnifiedTopology: true});
 
-const db = mongoose.connection;
-db.on('error' , console.error.bind(console, 'connection error'));
-db.once('open' , () => console.log('Conntected to database!'));
+// const db = mongoose.connection;
+// db.on('error' , console.error.bind(console, 'connection error'));
+// db.once('open' , () => console.log('Conntected to database!'));
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:<password>@test.ijzqf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
